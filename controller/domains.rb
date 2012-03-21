d31424b094d8e2de24def70334b71dc77b79fe30
Michel Blanc <mb@mbnet.fr>
2012-03-21 07:30:12 +0100

    Adds domains pagination
    Domain index page now has pagination

--- controller/domains.rb
+++ controller/domains.rb
@@ -2,13 +2,28 @@
 # Controller for Domains
 #
 
-# TODO: addinga domain could be nice !
+# TODO: adding a domain could be nice !
 #
 class Domains < MainController
   
   def index
     @title = 'Domains'
-    @domains = Domain
+
+    # Get params, filtering ou nil and turning them to symbol
+    sb = request.params['sortby'].nil? ? :name : request.params['sortby'].to_sym
+    od = request.params['order'].nil? ? :asc : request.params['order'].to_sym
+
+    # Check that the symbol obtained is valid
+    sb = :name unless [:type, :content, :ttl].include? sb
+    od = :asc unless :desc == od
+
+    if (:desc == od) then
+      Ramaze::Log.info("Sorting by #{sb} desc")
+      @domains = paginate(Domain.order(sb).reverse.all)
+    else
+      Ramaze::Log.info("Sorting by #{sb} asc")
+      @domains = paginate(Domain.order(sb).all)
+    end
   end
 
   def edit(id)
